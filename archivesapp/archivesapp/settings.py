2c02792bb8d54c778ff5b514bb1b099426fdb94a
s0595557 <Kabir.Lohana@Student.HTW-Berlin.de>
2024-12-02 20:11:50 +0100

    setting up database

--- archivesapp/archivesapp/settings.py
+++ archivesapp/archivesapp/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'archivesapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'archivesapp',
+        'USER': 'developeradmin',
+        'PASSWORD': 'archivespassword1',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
